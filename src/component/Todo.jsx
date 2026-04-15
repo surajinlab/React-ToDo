3b5ef2bed76d8f7902b07dbb733d9d7160173ef7
surajinlab <suraj.in.lab@gmail.com>
2026-04-15 23:39:34 +0530

    completed

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -11,8 +11,27 @@ const Todo = () => {
                     <figcaption className='caption'>Add Your List Here!</figcaption>
                 </figure>
                 <div>
-                    <input type="text" placeholder="Add item here..." className='form-control' />
+                    <input type="text" placeholder=" Add item here..." className='form-control' />
+                    <i className="fa fa-plus add-btn"></i>
                 </div>
+
+                    <div className='showItems'>
+                        <div className='eachItem'>
+                            <h3>Apple</h3>
+                            <div className='todo-btn'>
+                                <i className="fa fa-plus add-btn"></i>
+                                <i className="fa fa-plus add-btn"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button className='btn effect04' data-sm-link-text="Remove All">
+                            <span> CHECK LIST </span>
+                        </button>
+                    </div>
+                    
+                
             </div>
         </div>
     </>
